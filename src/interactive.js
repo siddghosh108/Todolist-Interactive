aec250a78d73a78495a055909d1b4cb2adf20e9d
Siddhartha Ghosh <sghosh@dview.ai>
2023-08-17 15:12:52 +0530

    Changed interactive.js

--- src/interactive.js
+++ src/interactive.js
@@ -2,7 +2,7 @@ import { storageInfo } from './addandremove.js';
 
 export function cleartasksDone(e) {
   let tempStore = storageInfo();
-  const updatedTasks = tempStore.filter(data => !data.completed);
+  const updatedTasks = tempStore.filter((data) => !data.completed); // Added parentheses around "data"
 
   localStorage.setItem('TasksInfo', JSON.stringify(updatedTasks));
 
